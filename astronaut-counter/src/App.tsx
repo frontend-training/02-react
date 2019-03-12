993a8b6f1cb8927780128b0e71ca26af54e9fa58
cris7ian <cristiancaroli@gmail.com>
2019-03-12 15:40:49 +0100

    skeleton

--- astronaut-counter/src/App.tsx
+++ astronaut-counter/src/App.tsx
@@ -1,22 +1,42 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <>
+                <ul className="astronaut-list list-group mb-3 col-md-4 ">
+                    <li className="list-group-item d-flex justify-content-between lh-condensed">
+                        <div>
+                            <h6 className="my-0">Oleg Kononenko</h6>
+                            <small className="text-muted">ISS</small>
+                        </div>
+                        <span className="text-muted">🚀</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between lh-condensed">
+                        <div>
+                            <h6 className="my-0">David Saint-Jacques</h6>
+                            <small className="text-muted">ISS</small>
+                        </div>
+                        <span className="text-muted">🚀</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between lh-condensed">
+                        <div>
+                            <h6 className="my-0">Anne McClain</h6>
+                            <small className="text-muted">ISS</small>
+                        </div>
+                        <span className="text-muted">🚀</span>
+                    </li>
+                </ul>
+                <div className="earth">
+                    <div className="earth-shadow"/>
+                </div>
+                <div className="moon"/>
+                <div className="stars"/>
+                <div className="stars-2"/>
+            </>
+        );
+    }
 }
 
 export default App;
