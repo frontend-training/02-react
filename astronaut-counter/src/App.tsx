89d00a0fa20d4b40aa93359b410df41b5c4bba87
cris7ian <cristiancaroli@gmail.com>
2019-03-12 15:50:56 +0100

    styles

--- astronaut-counter/src/App.tsx
+++ astronaut-counter/src/App.tsx
@@ -5,29 +5,30 @@ class App extends React.Component {
     public render() {
         return (
             <>
-                <ul className="astronaut-list list-group mb-3 col-md-4 ">
-                    <li className="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 className="my-0">Oleg Kononenko</h6>
-                            <small className="text-muted">ISS</small>
-                        </div>
-                        <span className="text-muted">🚀</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 className="my-0">David Saint-Jacques</h6>
-                            <small className="text-muted">ISS</small>
-                        </div>
-                        <span className="text-muted">🚀</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 className="my-0">Anne McClain</h6>
-                            <small className="text-muted">ISS</small>
-                        </div>
-                        <span className="text-muted">🚀</span>
-                    </li>
-                </ul>
+                <div className="astronaut-list">
+                    <h1>Astronauts in Space</h1>
+                    <ul className="list-group mb-3 col-md-3">
+                        <li className="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                                <h6 className="my-0">Oleg Kononenko</h6>
+                                <small className="text-muted">ISS</small>
+                            </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                                <h6 className="my-0">David Saint-Jacques</h6>
+                                <small className="text-muted">ISS</small>
+                            </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                                <h6 className="my-0">Anne McClain</h6>
+                                <small className="text-muted">ISS</small>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
                 <div className="earth">
                     <div className="earth-shadow"/>
                 </div>
